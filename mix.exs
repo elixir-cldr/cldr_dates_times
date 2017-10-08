5c6ab41b1ba96b8663033b2326e746cd518baf02
Kip Cole <kipcole9@gmail.com>
2017-10-08 08:45:36 +0800

    Add src to hex package contents

--- mix.exs
+++ mix.exs
@@ -1,21 +1,20 @@
 defmodule CldrDatesTimes.Mixfile do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.0"
 
   def project do
     [
       app: :ex_cldr_dates_times,
       version: @version,
-      name: "Cldr Dates Times",
+      name: "Cldr_Dates_Times",
       source_url: "https://github.com/kipcole9/cldr_dates_times",
       docs: docs(),
       elixir: "~> 1.5",
       description: description(),
       package: package(),
       start_permanent: Mix.env == :prod,
-      deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env)
+      deps: deps()
     ]
   end
 
@@ -41,10 +40,9 @@ defmodule CldrDatesTimes.Mixfile do
 
   defp deps do
     [
-      {:ex_cldr, path: "../cldr", override: true},
-      {:ex_cldr_numbers, "~> 0.1.0"},
-      {:ex_doc, ">= 0.0.0", optional: true, only: :dev},
-      {:stream_data, ">= 0.3.0", only: :test}
+      {:ex_cldr, "~> 0.6.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:stream_data, ">= 0.1.1", only: :test}
     ]
   end
 
@@ -54,7 +52,7 @@ defmodule CldrDatesTimes.Mixfile do
       licenses: ["Apache 2.0"],
       links: links(),
       files: [
-        "lib", "config", "mix.exs", "README*", "CHANGELOG*", "LICENSE*"
+        "lib", "src", "config", "mix.exs", "README*", "CHANGELOG*", "LICENSE*"
       ]
     ]
   end
@@ -62,13 +60,8 @@ defmodule CldrDatesTimes.Mixfile do
   def links do
     %{
       "GitHub"    => "https://github.com/kipcole9/cldr_dates_times",
-      "Readme"    => "https://github.com/kipcole9/cldr_dates_times/blob/v#{@version}/README.md",
       "Changelog" => "https://github.com/kipcole9/cldr_dates_times/blob/v#{@version}/CHANGELOG.md"
     }
   end
-
-  defp elixirc_paths(:test), do: ["lib", "mix", "test"]
-  defp elixirc_paths(:dev),  do: ["lib", "mix"]
-  defp elixirc_paths(_),     do: ["lib"]
 
 end
