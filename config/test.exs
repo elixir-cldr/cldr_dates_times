72f09381b7f4925fabc2dc42efb006c8611fa957
Kip Cole <kipcole9@gmail.com>
2017-11-02 16:40:10 +0900

    Lengthen test case load timeout value

--- config/test.exs
+++ config/test.exs
@@ -8,6 +8,6 @@ config :ex_cldr,
   precompile_transliterations: [{:latn, :arab}, {:arab, :thai}, {:arab, :latn}]
 
 config :ex_unit,
-  case_load_timeout: 220_000,
+  module_load_timeout: 220_000,
   timeout: 120_000
 
