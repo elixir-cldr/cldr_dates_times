81b3b15071ef661cd2f406ab3fff4aae2fbf8edc
Kip Cole <kipcole9@gmail.com>
2020-04-10 08:00:58 +0800

    Make protocol test more version resilient

--- test/cldr_chars_test.exs
+++ test/cldr_chars_test.exs
@@ -9,11 +9,14 @@ defmodule Cldr.DateTime.CharsTest do
     assert Cldr.to_string(~T[11:45:23]) == "11:45:23 AM"
   end
 
-  test "datetime to_string" do
-    assert Cldr.to_string(~U[2020-04-09 23:39:25.040129Z]) == "Apr 9, 2020, 11:39:25 PM"
-  end
-
   test "naive datetime to_string" do
     assert Cldr.to_string(~N[2020-04-09 23:39:25]) == "Apr 9, 2020, 11:39:25 PM"
   end
+
+  if Version.match?(System.version(), "~> 1.9") do
+    test "datetime to_string" do
+      assert Cldr.to_string(~U[2020-04-09 23:39:25.040129Z]) == "Apr 9, 2020, 11:39:25 PM"
+    end
+  end
+
 end
