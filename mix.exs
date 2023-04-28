a0871a43ddf7e47394748b114c77d28decc1ce62
Kip Cole <kipcole9@gmail.com>
2023-04-28 10:07:13 +0800

    Update deps

--- mix.exs
+++ mix.exs
@@ -69,9 +69,10 @@ defmodule Cldr.DatesTimes.Mixfile do
 
   defp deps do
     [
-      {:ex_cldr_numbers, path: "../cldr_numbers", override: true},
-      # {:ex_cldr_numbers, "~> 2.28"},
+      # {:ex_cldr_numbers, "~> 2.31"},
+      # {:ex_cldr_calendars, "~> 1.22"},
 
+      {:ex_cldr_numbers, path: "../cldr_numbers", override: true},
       {:ex_cldr_calendars, "~> 1.18"},
 
       {:calendar_interval, "~> 0.2", optional: true},
